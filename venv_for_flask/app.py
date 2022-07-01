431c2b309586df9d5877af213977a2ebbb3ceafa
Pavel Niakhai <3055963@mail.ru>
2022-07-01 17:03:30 +0300

    add file for give donations

--- venv_for_flask/app.py
+++ venv_for_flask/app.py
@@ -1,19 +1,26 @@
-from flask import Flask
+import json
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/request/donate', methods=['POST'])
 def make_donate():
+    with open('data.json', 'r') as f:
+        cont = json.load(f)
+    row_dict = {'name': request.form['donation_item'], 'amount': request.form['donation_amount']}
+    cont.append(row_dict)
+    with open('data.json', 'w') as f:
+        json.dump(cont, f)
     return """
     <html>
        <body>
-         <form action='/' method='post'>
-           <label> Пожертвование </label> 
-           <input> 
+         <form action='/request/donate' method='post'>            
+           <input id='donation_item' placeholder='Пожертвование'> 
+           </br>           
+           <input id='donation_amount' placeholder='Количество'>
            </br>
-           <label> Количество </label> 
-           <input>
+           <button type='submit'> Пожертововать </button> 
            </br>
-           <button type='submit'> Пожертововать </button>
+           <a href='/'> Вернуться на главную страницу </a>
          </form>
        </body>
     </html
@@ -24,7 +31,7 @@ def request_donate():
     return """
     <html>
        <body>
-         <form action='/' method='post'>           
+         <form action='/ask/donate' method='post'>           
            <label> Регистрация Прошения </label>            
            </br>
            <button type='submit'>            
@@ -41,5 +48,36 @@ def request_donate():
        </body>
     </html
     """
+@app.route('/ask/donate', methods=['POST'])
+def give_donate():
+    with open('data.json', 'r') as f:
+        cont = json.load(f)
+
+    if not cont:
+        return """
+        <html>
+          <body>
+            <form action='/' method='post'>
+              <p> У нас ничего нет для вас, заходите позже </p>
+              <a href='/'> Вернуться на главную страницу </a>
+            </form>
+          </body>
+        </html>
+        """
+    item = cont.pop()
+    with open('data.json', 'w') as f:
+        json.dump(cont, f)
+    return f'{item["name"]}' """
+        <html>
+          <body>
+            <form action='/' method='post'>
+              <a href='/'> Вернуться на главную страницу </a>
+            </form>
+          </body>
+        </html>
+        """
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
