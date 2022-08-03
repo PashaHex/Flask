d9ff0557e435ee40e4ae1430662f8c9b58fb18bc
Pavel Niakhai <3055963@mail.ru>
2022-08-03 15:32:16 +0300

    django project works with some problems

--- Django/donations/views.py
+++ Django/donations/views.py
@@ -20,79 +20,40 @@ def make_donate(request):
         'main_url': reverse('donations:main'),
         'thank_for_donate_url': reverse('donations:thank_for_donate')
     }
-    with open('data.json', 'r') as f:
-        cont = json.load(f)
+    with open('data.json', 'r') as file:
+        cont = json.load(file)
 
     item = cont.append(
         {'name': request.POST['donation_item'], 'amount': request.POST['donation_amount']}
     )
-    with open('data.json', 'w') as f:
-        json.dump(cont, f)
+    with open('data.json', 'w') as file:
+        json.dump(cont, file)
     return render(request, 'thank_for_donate.html', context)
 
 
 def list(request):
-    # context{}
-    with open ('data.json', 'r') as f:
-        context['f'] = json.load(f)
-    return render(request, 'list.html')
+    context = {}
+    with open('data.json', 'r') as file:
+        context['file'] = json.load(file)
+    return render(request, 'list.html', context)
 
 def ask_donate(request):
-    context = {
-        'main_url': reverse('donations:main'),
-        'empty_cont_url': reverse('donations:empty_cont'),
-        'full_url': reverse('donations:full_cont')
-    }
-    # item = None
-    with open('data.json', 'r') as f:
-        cont = json.load(f)
+    # context = {
+    #     'main_url': reverse('donations:main'),
+    #     'empty_cont_url': reverse('donations:empty_cont'),
+    #     'full_url': reverse('donations:full_cont')
+    # }
+    item = None
+    with open('data.json', 'r') as file:
+        cont = json.load(file)
 
-    if not cont or request.method != 'POST':
-        return render(request, 'empty_cont.html', context)  # , {'item': item})
-        # return HttpResponse(  f'''
-        #         <html>
-        #           <body>
-        #             <h3> Ничего нет для вас </h3>
-        #             <a href='/'> Back to main </a>
-        #           </body>
-        #         </html>
-        #         '''        )
+    if cont and request.method == 'POST':
+        item = cont.pop()
 
-    item = cont.pop()
-    with open('data.json', 'w') as f:
-        json.dump(cont, f)
-    return render(request, 'full_cont.html', context) #, item=item)
-    # return HttpResponse(        f'''
-    #     <html>
-    #       <body>
-    #         <h3> Вот вам, возьмите </h3>
-    #         {item['name']} {item['amount']}
-    #         <a href='/'> Back to main </a>
-    #       </body>
-    #     </html>
-    #     '''    )
+        with open('data.json', 'w') as file:
+            json.dump(cont, file)
+    return render(request, 'ask_complete.html', {'item': item})
 
-    # if cont:
-    #     item = cont.pop()
-    #     with open('data.json', 'w') as f:
-    #         json.dump(cont, f)
-    # return render(request, 'empty_cont.html') #, {'item': item})
-
-    # with open('data.json', 'r') as f:
-    #     data = f.read()
-    #     if data:
-    #         cont = json.load(data)
-    #     else:
-    #         cont = []
-    #
-    # if not cont:
-    #     return render(request, 'empty_cont.html', context)
-    #
-    # item = cont.pop()
-    # with open('data.json', 'w') as f:
-    #     json.dump(cont, f)
-    #
-    # return render(request, 'full_cont.html', item=item)
 
 def thank_for_donate(request):
     context = {
