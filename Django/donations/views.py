2ef8b4a50168efdfc7adcada3ab81a0e8b99d3aa
Pavel Niakhai <3055963@mail.ru>
2022-08-04 17:33:14 +0300

    remake took information from json file to DB in django prodlect

--- Django/donations/views.py
+++ Django/donations/views.py
@@ -5,54 +5,63 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse
 
+from donations.models import Request_donate
+
 
 def main_donate_page(request):
     context = {
         'make_donate_url': reverse('donations:make_donate'),
-        'ask_donate_url': reverse('donations:ask_donate'),
-        # 'main_url': reverse('donations:main')
+        'ask_donate_url': reverse('donations:ask_donate')
     }
     return render(request, 'donate.html', context)
 
 
 def make_donate(request):
-    context = {
-        'main_url': reverse('donations:main'),
-        'thank_for_donate_url': reverse('donations:thank_for_donate')
-    }
-    with open('data.json', 'r') as file:
-        cont = json.load(file)
-
-    item = cont.append(
-        {'name': request.POST['donation_item'], 'amount': request.POST['donation_amount']}
+    Request_donate.objects.create(
+        name=request.POST['donation_item'],
+        amount=request.POST['donation_amount']
     )
-    with open('data.json', 'w') as file:
-        json.dump(cont, file)
-    return render(request, 'thank_for_donate.html', context)
+    # context = {
+    #     'main_url': reverse('donations:main'),
+    #     'thank_for_donate_url': reverse('donations:thank_for_donate')
+    # }
+    # with open('data.json', 'r') as file:
+    #     cont = json.load(file)
+    #
+    # item = cont.append(
+    #     {'name': request.POST['donation_item'], 'amount': request.POST['donation_amount']}
+    # )
+    # with open('data.json', 'w') as file:
+    #     json.dump(cont, file)
+    return render(request, 'thank_for_donate.html', {'main_url': reverse('donations:main')})
 
 
 def list(request):
-    context = {}
-    with open('data.json', 'r') as file:
-        context['file'] = json.load(file)
+    context = {'file': Request_donate.objects.all()}
     return render(request, 'list.html', context)
 
+    # context = {}
+    # with open('data.json', 'r') as file:
+    #     context['file'] = json.load(file)
+    # return render(request, 'list.html', context)
+
 def ask_donate(request):
-    # context = {
-    #     'main_url': reverse('donations:main'),
-        # 'empty_cont_url': reverse('donations:empty_cont'),
-        # 'full_url': reverse('donations:full_cont')
-    # }
-    item = None
-    with open('data.json', 'r') as file:
-        cont = json.load(file)
+    item = Request_donate.objects.order_by('id').filter(state=False).first()
+    if item:
+        item.state = True
+        item.save()
+    return render(request, 'ask_complete.html', {'item': item, 'main_url': reverse('donations:main')})
 
-    if cont and request.method == 'POST':
-        item = cont.pop()
-
-        with open('data.json', 'w') as file:
-            json.dump(cont, file)
-    return render(request, 'ask_complete.html',  {'item': item, 'main_url': reverse('donations:main')})
+    # item = None
+    # with open('data.json', 'r') as file:
+    #     cont = json.load(file)
+    #
+    # if cont and request.method == 'POST':
+    #     item = cont.pop()
+    #
+    #     with open('data.json', 'w') as file:
+    #         json.dump(cont, file)
+    # return render(request, 'ask_complete.html',  {'item': item, 'main_url': reverse('donations:main')})
 
 
 def thank_for_donate(request):
