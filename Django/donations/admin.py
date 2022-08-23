d8fdb15abdf4e31320dd602f9236b69580659068
Pavel Niakhai <3055963@mail.ru>
2022-08-23 17:08:24 +0300

    HomeWork 22 fix migrate

--- Django/donations/admin.py
+++ Django/donations/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Request_donate
+from .models import Request_donate, Office
 
 class DonateAdmin(admin.ModelAdmin):
-    fields = ['ask', 'make donate']
+    exclude = ['id']
+    # fields = ['ask', 'make donate']
 
 admin.site.register(Request_donate, DonateAdmin)
+
+class OfficeAdmin(admin.ModelAdmin):
+    exclude = ['id']
+    # fields = ['name', 'capacity', 'occupied']
+
+admin.site.register(Office, OfficeAdmin)
